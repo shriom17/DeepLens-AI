26e1172d852e557885f95549a7c2db0ff53d62ec
shriom17 <129147531+shriom17@users.noreply.github.com>
2026-08-22 13:27:07 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from backend.routes.auth import router as auth_router
 from backend.routes.analyze import router as analyze_router
 
-
 app = FastAPI(
     title="NoticeSense AI API",
     description="Backend API for NoticeSense AI",
     version="1.0.0"
 )
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,10 +20,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 app.include_router(auth_router)
 app.include_router(analyze_router)
-
 
 @app.get("/")
 def home():
